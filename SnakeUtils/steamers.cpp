841cfc01b4bcf815ea64c73404b57b956c98009d
EndrII <EndrIIMail@gmail.com>
2019-02-20 17:38:27 +0300

    streamer

--- SnakeUtils/steamers.cpp
+++ SnakeUtils/steamers.cpp
@@ -21,7 +21,27 @@ bool Steamers::baseWrite(QDataStream &stream, const QVariantMap &map) {
 bool Steamers::read(QDataStream &stream, QVariantMap &map, Steamers::Type type) {
     switch (type) {
     case Snake : {
-        return true;
+        if (baseRead(stream, map)) {
+            return false;
+        };
+
+        unsigned char spead;
+        QList<float> skillet;
+
+        stream >> spead;
+        stream >> skillet;
+        QVariantList varList;
+
+        for (auto i : skillet) {
+            varList.push_back(i);
+        }
+
+        map["class"] = Snake;
+        map["spead"] = spead;
+        map["skillet"] = varList;
+
+        return stream.status() == QDataStream::Ok ||
+               stream.status() == QDataStream::ReadPastEnd;
     }
 
     case Player : {
@@ -74,7 +94,22 @@ bool Steamers::read(QDataStream &stream, QVariantMap &map, Steamers::Type type) 
 bool Steamers::write(QDataStream &stream, const QVariantMap &map, Steamers::Type type) {
     switch (type) {
     case Snake : {
-        return true;
+        if (baseWrite(stream, map)) {
+            return false;
+        };
+
+        stream << static_cast<quint8>(map.value("spead").toUInt());
+
+        QList<float> skillet;
+        auto varList = map.value("skillet").toList();
+        for (auto &&i : varList) {
+            skillet.push_back(i.toFloat());
+        }
+
+        stream << skillet;
+
+        return stream.status() == QDataStream::Ok ||
+               stream.status() == QDataStream::ReadPastEnd;
     }
 
     case Player : {
