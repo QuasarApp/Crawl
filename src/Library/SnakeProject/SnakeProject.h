6ed6fc65551f8457d283be15ef16e35d9bd35818
EndrII <EndrIIMail@gmail.com>
2021-05-25 12:11:49 +0300

    ref #30 new structure ("using QuasarApp Cmake Project")

---
+++ src/Library/SnakeProject/SnakeProject.h
@@ -0,0 +1,16 @@
+//#
+//# Copyright (C) 2021-2021 QuasarApp.
+//# Distributed under the lgplv3 software license, see the accompanying
+//# Everyone is permitted to copy and distribute verbatim copies
+//# of this license document, but changing it is not allowed.
+//#
+
+#include "SnakeProject_global.h"
+
+inline void initSnakeProjectResources() { Q_INIT_RESOURCE(SnakeProject); }
+
+namespace SnakeProject {
+
+bool SnakeProject_EXPORT init();
+
+};
