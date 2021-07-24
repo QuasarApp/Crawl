9ae1a58c891cbd0eed446e0f4927d7a71eb94d2a
Igor loschinin <igor.loschinin2014@yandex.ru>
2021-07-24 19:26:13 +0300

    Update src/CrawlAbstractLvl/private/obstaclered.cpp
    Co-authored-by: Andrei Yankovich <EndrIIMail@gmail.com>

--- src/CrawlAbstractLvl/private/obstaclered.cpp
+++ src/CrawlAbstractLvl/private/obstaclered.cpp
@@ -11,7 +11,7 @@ namespace AbstractLvl {
 
 ObstacleRed::ObstacleRed() : IWorldItem(AUTO_CLASS_NAME) {
     setMash("qrc:/mesh/meshes/ObstacleRed.mesh");
-    setSize({2,2,2});
+    setSize({1,1,1});
     setColor("#ff1927");
     setZ(0);
 }
